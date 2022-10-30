4aa2c7e5d2eead0902fd7a83fa284154fd22f4b9
Hersonls <hersonls@gmail.com>
2022-10-30 16:26:26 -0300

    Add missing const/let in main.js

--- main.js
+++ main.js
@@ -1,110 +1,106 @@
-(function(window, document) {
-  document.getElementById('form').addEventListener('submit', onSubmit)
+(function (window, document) {
+  document.getElementById('form').addEventListener('submit', onSubmit);
 
-  function onSubmit (event) {
-    event.preventDefault()
-  
-    const htmlCode = getHtmlCode()
-  
-    if(!htmlCode) {
-      return
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const htmlCode = getHtmlCode();
+
+    if (!htmlCode) {
+      return;
     }
-  
-    const codeReport = generateCodeReport(htmlCode)
-    renderResults(codeReport)
+
+    const codeReport = generateCodeReport(htmlCode);
+
+    renderResults(codeReport);
   }
-  
-  function getHtmlCode () {
-    return document.getElementById('code').value
+
+  function getHtmlCode() {
+    return document.getElementById('code').value;
   }
-  
-  function generateCodeReport (html) {
-    const report = getReport(html)
-    const proportionality = calculateGeneralProportionality(report)
-  
+
+  function generateCodeReport(html) {
+    const report = getReport(html);
+    const proportionality = calculateGeneralProportionality(report);
+
     return {
       report,
-      proportionality
-    }
+      proportionality,
+    };
   }
-  
-  function getReport (html) {
-    var doc = new DOMParser().parseFromString(html, "text/html");
-  
-    allTags = doc.getElementsByTagName('*')
-    allTagsCount = allTags.length
-  
-    report = {}
-  
+
+  function getReport(html) {
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    const allTags = doc.getElementsByTagName('*');
+    const report = {};
+
     for (let index = 0; index < allTags.length; index++) {
       const element = allTags[index];
-  
-      tagName = element.tagName.toLowerCase()
-      text = extractText(element)
-  
-      outerHtml = formatText(element.outerHTML)
-      innerHTML = formatText(element.innerHTML)
-      tagLength = outerHtml.length - innerHTML.length
-  
+      const tagName = element.tagName.toLowerCase();
+      const text = extractText(element);
+      const outerHtml = formatText(element.outerHTML);
+      const innerHTML = formatText(element.innerHTML);
+      const tagLength = outerHtml.length - innerHTML.length;
+
       report[tagName] = {
         count: (report[tagName]?.count || 0) + 1,
-        tagLength: (report[tagName]?.tagLength  || 0) + tagLength,
-        textLength: (report[tagName]?.textLength || 0) + text.length
-      }
+        tagLength: (report[tagName]?.tagLength || 0) + tagLength,
+        textLength: (report[tagName]?.textLength || 0) + text.length,
+      };
     }
-  
-    return report
+
+    return report;
   }
-  
+
   function extractText(element) {
-    excludedElements = ['script', 'style']
-    
+    excludedElements = ['script', 'style'];
+
     if (excludedElements.indexOf(element.tagName.toLowerCase()) === -1) {
       text = '';
       for (var i = 0; i < element.childNodes.length; ++i)
         if (element.childNodes[i].nodeType === Node.TEXT_NODE)
           text += element.childNodes[i].textContent;
-      return formatText(text)
+      return formatText(text);
     }
-    
-    return ''
+
+    return '';
   }
-  
-  function calculateGeneralProportionality (report) {
-    totalTagLength = 0
-    totalTextLength = 0
-  
+
+  function calculateGeneralProportionality(report) {
+    let totalTagLength = 0;
+    let totalTextLength = 0;
+
     for (const tagReport of Object.values(report)) {
-      totalTagLength += tagReport.tagLength
-      totalTextLength += tagReport.textLength
+      totalTagLength += tagReport.tagLength;
+      totalTextLength += tagReport.textLength;
     }
-  
-    const totalLength = totalTagLength + totalTextLength
-    const contentPercentage = getPercentage(totalTextLength, totalLength)
-    const codePercentage = getPercentage(totalTagLength, totalLength)
-  
+
+    const totalLength = totalTagLength + totalTextLength;
+    const contentPercentage = getPercentage(totalTextLength, totalLength);
+    const codePercentage = getPercentage(totalTagLength, totalLength);
+
     return {
       content: contentPercentage,
-      code: codePercentage
-    }
+      code: codePercentage,
+    };
   }
-  
-  function formatText (text) {
-    return text.replace(/\n|(\s\s+)/g, '').trim()
+
+  function formatText(text) {
+    return text.replace(/\n|(\s\s+)/g, '').trim();
   }
-  
-  function getPercentage (value, total) {
-    const percentage = value / total * 100
-    return percentage.toFixed(2)
+
+  function getPercentage(value, total) {
+    const percentage = (value / total) * 100;
+    return percentage.toFixed(2);
   }
-  
-  function renderResults ({ proportionality, report }) {
+
+  function renderResults({proportionality, report}) {
     const proportionalityText = `
       <h2>General proportionality:</h2>
       <p><strong>Code:</strong> ${proportionality.code}%</p>
       <p><strong>Content:</strong> ${proportionality.content}%</p>
-    `
-  
+    `;
+
     const tagsText = `
       <h2>Tags:</h2>
       <table>
@@ -115,16 +111,21 @@
           </tr>
         </thead>
         <tbody>
-          ${Object.entries(report).map(([tag, data]) => `
+          ${Object.entries(report)
+            .map(
+              ([tag, data]) => `
             <tr>
               <td><strong>${tag}</strong></td>
               <td>${data.count}</td>
             </tr>
-          `).join('\n')}
+          `,
+            )
+            .join('\n')}
         </tbody>
       </table>
-    `
-  
-    document.getElementById('results').innerHTML = proportionalityText + tagsText
+    `;
+
+    document.getElementById('results').innerHTML =
+      proportionalityText + tagsText;
   }
-})(window, document)
+})(window, document);
